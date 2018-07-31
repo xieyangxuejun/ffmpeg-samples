1386bd7ac556cb672c0de01b679776afa05b2401
xieyangxuejun <xieyangxuejun@gmail.com>
2018-07-31 20:18:09 +0800

    init

---
+++ app/src/main/java/com/foretree/samples/ffmpeg/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.foretree.samples.ffmpeg
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.foretree.ffmpeg.FFmpegCmd
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text = FFmpegCmd.getFFmpegConfig()
+    }
+
+    /**
+     * 从视频中提取视频.不要音频
+     */
+    fun getVideoFrom(inPath: String, outPath: String) : String{
+        //ffmpeg -i test.mp4 -an -y -vcodec copy out.mp4
+        return String.format("ffmpeg -i %s -an -y -vcodec copy %s", inPath, outPath).let {
+            FFmpegCmd.run(it)
+            outPath
+        }
+    }
+
+    /**
+     * 合并视频和音频
+     * 若音频比视频长，画面停留在最后一帧，继续播放声音。
+     */
+    fun mergeVideo(paths: Array<String>, outPath: String) : String{
+        return StringBuffer().apply {
+            for (path in paths) {
+                this.append("-i")
+                this.append(path)
+            }
+            this.append("-c:a")
+            this.append("copy")
+            this.append("-c:v")
+            this.append("copy")
+
+        }.toString().let {
+            FFmpegCmd.run(it)
+            outPath
+        }
+    }
+}
---
+++ ffmpeg/src/main/java/com/foretree/ffmpeg/FFmpegCmd.java
@@ -0,0 +1,35 @@
+package com.foretree.ffmpeg;
+
+/**
+ * Created by silen on 2018/7/31 17:18
+ * Copyright (c) 2018 in FORETREE
+ */
+public class FFmpegCmd {
+
+    static {
+        System.loadLibrary("avutil");
+        System.loadLibrary("fdk-aac");
+        System.loadLibrary("avcodec");
+        System.loadLibrary("avformat");
+        System.loadLibrary("swscale");
+        System.loadLibrary("swresample");
+        System.loadLibrary("avfilter");
+        System.loadLibrary("jxffmpegrun");
+    }
+
+    /**
+     * 命令运行
+     * @param cmd
+     * @return
+     */
+    public static int run(String cmd){
+        String regulation="[ \\t]+";
+        final String[] split = cmd.split(regulation);
+
+        return ffmpegRun(split);
+    }
+
+    public static native int ffmpegRun(String[] cmd);
+
+    public static native String getFFmpegConfig();
+}
